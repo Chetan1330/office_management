76d600d1417817f5584ef816d4afc2db05496b71
Chetan1330 <1330chetan@gmail.com>
2022-07-17 14:53:17 +0530

    Add your secret key

--- Office_management/Office_management/settings.py
+++ Office_management/Office_management/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'bh=t%^2er)4+ssip1@v&v-9u8nmlk*f*5yguzh*yiy!pbu(9zh'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
